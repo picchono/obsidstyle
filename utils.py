84920034e42d02f7c76f30852972b1047c3bfc49
Pierre Levy <pierre_levy@outlook.com>
2023-11-26 18:22:46 +0100

    10

--- utils.py
+++ utils.py
@@ -292,7 +292,7 @@ class Settings:
         "SLUGIFY": "y",
         "HOME_GRAPH": "y",
         "PAGE_GRAPH": "y",
-        "SUBSECTION_SYMBOL": "👉",
+        "SUBSECTION_SYMBOL": "·",
         "LOCAL_GRAPH": "",
         "GRAPH_LINK_REPLACE": "",
         "STRICT_LINE_BREAKS": "y",
